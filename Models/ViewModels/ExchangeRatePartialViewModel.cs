8e248a8de03a89e4635c8b5d40e01c04c0cd4109
Artyom Voytenko <artemvoitenko58@gmail.com>
2021-01-24 10:40:28 +0200

    Add exchange rate for products(2)

---
+++ Models/ViewModels/ExchangeRatePartialViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models.ViewModels
+{
+    public class ExchangeRatePartialViewModel
+    {
+        public Product Product { get; set; }
+        public CurrencyConverter CurrencyConverter { get; set; }
+    }
+}
